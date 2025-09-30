37b235ae0aee1be4ef08b3feb01ecaac7dee7152
konusvykhuhol <fedasimonin@gmail.com>
2025-09-30 19:14:41 +0300

    main.py

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ class calculator(loader.Module):
     """Описание модуля"""
 
     strings = {"name": "calculator :D"}
-
-
+    
     async def sumcmd(self,message):
         """сумма двух чисел. вводить в формате .sum [число] [число]"""
         b=utils.get_args_raw(message)
         a=b.split(" ")
         if len(a)==2:
-            if a[1].isdigit() and a[2].isdigit():
+            if a[0].isdigit() and a[1].isdigit():
                 await utils.answer(message, f"сумма{a[1]} и {a[2]} =", a[1]+a[2])
             else:
                 await utils.answer(message, f"введены не числа!")
