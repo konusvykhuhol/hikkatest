65ccdb169f756a0b38ccd832d6082cad85eb393f
konusvykhuhol <fedasimonin@gmail.com>
2025-09-30 18:49:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,17 +4,14 @@ from .. import loader, utils  # type: ignore
 
 @loader.tds
 class ExampleModule(loader.Module):
-    """Описание модуля"""  # ЭТО ОБЯЗАТЕЛЬНО!
+    """Описание модуля"""
 
-    strings = {"name": "ExampleModule"}  # И ЭТО ТОЖЕ!
+    strings = {"name": "calculator :D"}
 
-    async def testcmd(self, message):
-        """Тестовая команда"""
-        await utils.answer(message, "✅ Модуль работает!")
 
     async def sumcmd(self,message):
         """сумма двух чисел. вводить в формате .sum [число] [число]"""
-        a=message.split(" ")
+        a=utils.get_args_raw(message)
         if len(a)==3:
             if a[1].typeof()==int and a[2].typeof()==int:
                 await utils.answer(message, f"сумма{a[1]} и {a[2]} =", a[1]+a[2])
