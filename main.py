6175489edeb887241161f487a826618d6558b0bb
konusvykhuhol <fedasimonin@gmail.com>
2025-09-30 18:40:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ class ExampleModule(loader.Module):
     async def testcmd(self, message):
         """Тестовая команда"""
         await utils.answer(message, "✅ Модуль работает!")
+
+    async def sumcmd(self,message):
+        """сумма двух чисел. вводить в формате .sum [число] [число]"""
+        a=message.split(" ")
+        if len(a)==3:
+            if a[1].typeof()==int and a[2].typeof()==int:
+                await utils.answer(message, f"сумма{a[1]} и {a[2]} =", a[1]+a[2])
+            else:
+                await utils.answer(message, f"введены не числа!")
+        else:
+            await utils.answer(message, f"неверный формат!")
