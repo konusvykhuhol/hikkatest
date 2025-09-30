2efabb210171b559beed448e373bb46389f72a8e
konusvykhuhol <fedasimonin@gmail.com>
2025-09-30 18:56:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ class ExampleModule(loader.Module):
 
     async def sumcmd(self,message):
         """сумма двух чисел. вводить в формате .sum [число] [число]"""
-        a=utils.get_args_raw(message)
+        b=utils.get_args_raw(message)
+        a=b.split()
         if len(a)==3:
             if a[1].typeof()==int and a[2].typeof()==int:
                 await utils.answer(message, f"сумма{a[1]} и {a[2]} =", a[1]+a[2])
