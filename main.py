8746edb95926caaa54eec6da0c8cfa948c16b430
konusvykhuhol <fedasimonin@gmail.com>
2025-09-30 19:19:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ class calculator(loader.Module):
     """Описание модуля"""
 
     strings = {"name": "calculator :D"}
-    
+
+
     async def sumcmd(self,message):
         """сумма двух чисел. вводить в формате .sum [число] [число]"""
         b=utils.get_args_raw(message)
         a=b.split(" ")
         if len(a)==2:
             if a[0].isdigit() and a[1].isdigit():
-                await utils.answer(message, f"сумма{a[0]} и {a[1]} =", a[0]+a[1])
+                await utils.answer(message, f"сумма{a[0]} и {a[1]} =", a[1]+a[0])
             else:
                 await utils.answer(message, f"введены не числа!")
         else:
             await utils.answer(message, f"неверный формат!")
-
