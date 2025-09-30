be634bf358f262d5329a94d5f1e691521e19fb83
konusvykhuhol <fedasimonin@gmail.com>
2025-09-30 19:04:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from .. import loader, utils  # type: ignore
 
 
 @loader.tds
-class ExampleModule(loader.Module):
+class calculator(loader.Module):
     """Описание модуля"""
 
     strings = {"name": "calculator :D"}
@@ -13,7 +13,7 @@ class ExampleModule(loader.Module):
         """сумма двух чисел. вводить в формате .sum [число] [число]"""
         b=utils.get_args_raw(message)
         a=b.split(" ")
-        if len(a)==3:
+        if len(a)==2:
             if a[1].typeof()==int and a[2].typeof()==int:
                 await utils.answer(message, f"сумма{a[1]} и {a[2]} =", a[1]+a[2])
             else:
